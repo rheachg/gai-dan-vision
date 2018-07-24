7a98b61e1bbfe65f60f5dfd49ded712ca8ec3184
Rhea Chugh <rhea@vivovii.com>
2018-07-24 10:31:21 +0800

    Initial Commit

---
+++ Gaidan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gaidan
+//
+//  Created by Rhea Chugh on 24/7/2018.
+//  Copyright © 2018 Rhea Chugh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
